b3c19bd0c82958b0624287a1e4c8b85582bdd034
AstridDunst <dun16134@spengergasse.at>
2017-03-23 16:21:21 +0100

    Vorbereitung für Romi

--- app/src/main/java/com/example/astridd/epa/frm_stammdaten.java
+++ app/src/main/java/com/example/astridd/epa/frm_stammdaten.java
@@ -48,6 +48,8 @@ public class frm_stammdaten extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+
+
     private View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {
 
         @Override
